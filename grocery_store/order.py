4d2acc64ca1bf43c52073bf9d27a7935f833ee76
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-09-16 00:56:13 -0700

    Fix logic and runtime errors

--- grocery_store/order.py
+++ grocery_store/order.py
@@ -12,7 +12,30 @@ def add_product(order, product):
 
 def calculate_total(order):
     total = 0
-    for i in range(1, len(order["products"])):
+    for i in range(0, len(order["products"])):
         total += order["products"][i]["price"]
 
     return total
+
+    # product_one = create_product("Trombone", 478.95) 
+    # product_two = create_product("Guitar", 327.99) 
+    # product_three = create_product("Triangle", 2) {
+    # products = [product_one, product_two, product_three]
+    # products = [
+    #   {"name": "trombone", "price": 478.95}, 
+    #   {"name": "guitar", "price": 327.99}, 
+    #   {"name": "triangle", "price": 2}
+    # ]
+    # order = create_order(products, "Shona Frederick")
+    # create_order = {
+    #   "customer name": "Shona Frederick", 
+    #   "products":
+    #       [
+    #   {"name": "trombone", "price": 478.95}, 
+    #   {"name": "guitar", "price": 327.99}, 
+    #   {"name": "triangle", "price": 2}
+    # ] }
+
+
+    # # Act
+    # total = calculate_total(order)
